0d2146324b76fa0bc2d12915dfd8615c082b979d
Siria Sadeddon <siriasadeddin@gmail.com>
2021-08-19 13:15:26 -0500

    Add files via upload

--- models/yolo.py
+++ models/yolo.py
@@ -83,28 +83,28 @@ class IDetect(nn.Module):
         self.register_buffer('anchor_grid', a.clone().view(self.nl, 1, -1, 1, 1, 2))  # shape(nl,1,na,1,1,2)
         self.m = nn.ModuleList(nn.Conv2d(x, self.no * self.na, 1) for x in ch)  # output conv
         
-        #self.ia = nn.ModuleList(ImplicitA(x) for x in ch)
-        #self.im = nn.ModuleList(ImplicitM(self.no * self.na) for _ in ch)
+        self.ia = nn.ModuleList(ImplicitA(x) for x in ch)
+        self.im = nn.ModuleList(ImplicitM(self.no * self.na) for _ in ch)
 
     def forward(self, x):
         # x = x.copy()  # for profiling
-        #z = []  # inference output
+        z = []  # inference output
         self.training |= self.export
-        #for i in range(self.nl):
-           # x[i] = self.im[i](self.m[i](self.ia[i](x[i])))  # conv
-           # bs, _, ny, nx = x[i].shape  # x(bs,255,20,20) to x(bs,3,20,20,85)
-           # x[i] = x[i].view(bs, self.na, self.no, ny, nx).permute(0, 1, 3, 4, 2).contiguous()
+        for i in range(self.nl):
+            x[i] = self.im[i](self.m[i](self.ia[i](x[i])))  # conv
+            bs, _, ny, nx = x[i].shape  # x(bs,255,20,20) to x(bs,3,20,20,85)
+            x[i] = x[i].view(bs, self.na, self.no, ny, nx).permute(0, 1, 3, 4, 2).contiguous()
 
-        #   if not self.training:  # inference
-        #        if self.grid[i].shape[2:4] != x[i].shape[2:4]:
-        #            self.grid[i] = self._make_grid(nx, ny).to(x[i].device)
+            if not self.training:  # inference
+                if self.grid[i].shape[2:4] != x[i].shape[2:4]:
+                    self.grid[i] = self._make_grid(nx, ny).to(x[i].device)
 
-        #        y = x[i].sigmoid()
-        #        y[..., 0:2] = (y[..., 0:2] * 2. - 0.5 + self.grid[i]) * self.stride[i]  # xy
-        #        y[..., 2:4] = (y[..., 2:4] * 2) ** 2 * self.anchor_grid[i]  # wh
-        #        z.append(y.view(bs, -1, self.no))
-        return x 
-        #if self.training else (torch.cat(z, 1), x)
+                y = x[i].sigmoid()
+                y[..., 0:2] = (y[..., 0:2] * 2. - 0.5 + self.grid[i]) * self.stride[i]  # xy
+                y[..., 2:4] = (y[..., 2:4] * 2) ** 2 * self.anchor_grid[i]  # wh
+                z.append(y.view(bs, -1, self.no))
+
+        return x if self.training else (torch.cat(z, 1), x)
 
     @staticmethod
     def _make_grid(nx=20, ny=20):
